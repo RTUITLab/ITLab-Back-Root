47fb928abd60a9ce609a748273f62d7c88486a71
Maksim Makushchenko <maksalmak@gmail.com>
2020-11-10 16:10:25 +0300

    Update alias

--- alias-back.ps1
+++ alias-back.ps1
@@ -1,32 +1,12 @@
-$baseFiles = @() + (Get-ChildItem -Path . -Include docker-compose-files.txt -Force -Recurse | ForEach-Object {$_.FullName})
-
-$devFiles = @() + (Get-ChildItem -Path . -Include docker-compose-files.development.txt -Force -Recurse | ForEach-Object {$_.FullName})
-$prodFiles = @() + (Get-ChildItem -Path . -Include docker-compose.production-files.txt -Force -Recurse | ForEach-Object {$_.FullName})
-
-$devAllFiles =  @() + ($baseFiles + $devFiles | Where-Object {$_})
-$prodAllFiles = @() + ($baseFiles + $prodFiles | Where-Object {$_})
-
-$currentDev = Get-Content $devAllFiles | ForEach-Object { Resolve-Path -Path $_ } | ForEach-Object { "-f $_" }
-$currentProd = Get-Content $prodAllFiles | ForEach-Object { Resolve-Path -Path $_ } | ForEach-Object { "-f $_" }
-
-Write-Output dev
-$currentDev
-
-Write-Host prod
-$currentProd
-
-$devDockerFiles = [string]::Join(" ", $currentDev)
-$prodDockerFiles = [string]::Join(" ", $currentProd)
-
-$devCommand = "docker-compose $devDockerFiles"
-$prodCommand = "docker-compose $prodDockerFiles"
-
-function DevelopmentBackendDockerCompose {
-       Invoke-Expression "$devCommand $args"
+function BackendDockerCompose {
+    docker-compose `
+        -f .\docker-compose.yml `
+        -f .\docker-compose.override.yml `
+ `
+        -f .\ITLab-Identity\docker-compose.yml `
+        -f .\ITLab-Identity\docker-compose.override.yml `
+ `
+        $args
 }
 
-function ProductionBackendDockerCompose {
-       Invoke-Expression "$prodCommand $args"
-}
-Set-Alias -Name bdc DevelopmentBackendDockerCompose
-Set-Alias -Name pbdc ProductionBackendDockerCompose
+Set-Alias -Name bdc BackendDockerCompose
